23f72ba98065586a661e84bbcaf4fdbc2ced5984
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-03-16 17:23:50 -0600

    creat DB and connections

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey and artichoke burger');
+INSERT INTO burgers (burger_name) VALUES ('Beef and blue cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken and vegetables burger');
